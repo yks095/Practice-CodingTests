3b43357ab951e398f5feb75abf0b9b30b46e6bdc
양기석 <owen@hinterofficial.com>
2021-11-23 18:46:30 +0900

    Practice Coding Test

---
+++ programmers/level1/Phonekemon.java
@@ -0,0 +1,16 @@
+//package programmers.level1;
+//
+///**
+// * 폰케몬
+// * https://programmers.co.kr/learn/courses/30/lessons/1845?language=java
+// */
+//public class Phonekemon {
+//    public static void main(String[] args) {
+//        int[] nums = {3,1,2,3};
+//        System.out.println(solution(nums));
+//
+//    }
+//
+//    private static int solution(int[] nums) {
+//    }
+//}
